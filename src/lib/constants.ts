a7ccd22f5fc57a8e86cc8a8f70aa79eee8c33023
Arvind Raghu <arvind.r03@gmail.com>
2024-09-14 23:27:40 +0100

    version 1.7 about

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -292,9 +292,11 @@ My key skills / takeaways from this project were:
 ]
 
 export const ABOUT_ME_MARKDOWN = `
-Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I've had the privilege of building software for an **advertising agency**, a **start-up**, a **huge corporation**, and a **digital product studio**.
+👋 Hey there! I'm Arvind, a **final year Computer Science student at the University of Cambridge**, and a **software / machine learning engineer**, having worked with startups such as **Paige Braille**, and big names such as **Revolut**, alike.
 
-My main focus these days is building accessible user interfaces for our customers at **Klaviyo**. I most enjoy building software in the sweet spot where design and engineering meet — things that look good but are also built well under the hood. In my free time, I've also released an **online video course** that covers everything you need to know to build a web app with the Spotify API.
+My introduction to computer science was when my dad showed me how he worked with financial databases in SQL Server when I was 12 years old (quite an unconventional first language). Since then, I've been creating projects and working in teams to **create an impact with my technological knowledge**.
 
-When I'm not at the computer, I'm usually rock climbing, reading, hanging out with my wife and two cats, or running around Hyrule searching for **Korok seeds**.
+Apart from programming, I am a **huge music fan** (🎵 my favourite album is The Off Season by J. Cole), I play **badminton**, and I am a **boxing enthusiast**!
+
+I'm always looking for opportunities to push myself outside of my comfort zone and create an impact, so **please get in touch** if you'd like to work together! If you'd like to learn more about my experience, have a look below, and reach out to me to find out more 😎
 `;
